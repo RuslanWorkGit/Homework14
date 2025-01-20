c0f8c2a6e09d160a0334a84eb36ac426e1b6a474
RuslanWorkGit <lulka758@gmail.com>
2025-01-20 21:18:09 +0200

    create empty project

---
+++ Homework14/Homework14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework14
+//
+//  Created by Ruslan Liulka on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
